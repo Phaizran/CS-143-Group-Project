aacffcb8d9159041056b5da11d9b934a75e7bc9a
Phaizran <57541255+Phaizran@users.noreply.github.com>
2020-02-11 11:05:33 -0800

    Add files via upload

---
+++ MazeTile.java
@@ -0,0 +1,14 @@
+
+public class MazeTile {
+	private boolean northIsWall;
+	private boolean eastIsWall;
+	private boolean southIsWall;
+	private boolean westIsWall;
+	
+	public MazeTile(int wallNumber) {
+		while (wallNumber > 0) {
+			//TODO randomly set up appropriate number of walls
+		}
+	}
+
+}
