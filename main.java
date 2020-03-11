66e09b3c23460e241769986854ed062a764b87ac
Aaron Eldridge <aceldridge7@gmail.com>
2020-03-11 11:01:59 -0700

    correect files

---
+++ main.java
@@ -0,0 +1,38 @@
+
+public class main {
+
+	public static void main(String[] args) {
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		MazeGenerator map = new MazeGenerator(10,10);
+		MazeSolver solver = new MazeSolver(map);
+		
+		map.printInfo();
+		
+		System.out.println(solver.solver());
+		boolean[][] solve = solver.getCorrectPath();
+		boolean[][] places = solver.getBefore();
+		for(int i = 0; i < map.length; i++)
+		{
+			for(int a = 0; a < map.width; a++)
+			{
+				System.out.print(solve[i][a] + ", ");
+			}
+			System.out.println();
+		}
+
+		
+		MazeGenerator maze = new MazeGenerator(10,10);
+		maze.printInfo();
+		MazeSolver solve = new MazeSolver("pass in the parameter");
+	}
+
+}
+
+	}
+
+}
